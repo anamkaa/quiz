330e196c68f4351e64821d3decbc7a658a62febf
anamkaa <75376715+anamkaa@users.noreply.github.com>
2022-08-31 12:33:28 +0530

    fixed score and color feature

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -3,18 +3,18 @@ import { useScore } from "../context/score-context";
 
 const QuestionCard = ({ question }) => {
   const { scoreState, scoreDispatch } = useScore();
-  const [bgColor, setBgColor] = useState(false);
+  const [bgColor, setBgColor] = useState({ q_no: undefined, color: "" });
+  const [disable, setDisable] = useState(false);
 
-  const scoreCalculator = (isTrue) => {
+  const scoreCalculator = (isTrue, q_no) => {
+    setDisable(true);
     if (isTrue === true) {
       scoreDispatch({ type: "INCREMENT", payload: 1 });
-      console.log("Yay, Correct");
+      setBgColor({ q_no, color: "green" });
     } else {
-      console.log("Nay, Wrong");
+      setBgColor({ q_no, color: "red" });
     }
   };
-
-  console.log(scoreState.score);
 
   return (
     <>
@@ -30,30 +30,38 @@ const QuestionCard = ({ question }) => {
           {question?.statement}
         </div>
 
-        <div
+        <button
           className="qz-qn-card-optn qz-qn-card-chip chip-hover"
-          onClick={() => scoreCalculator(question.option[0].isCorrect)}
+          onClick={() => scoreCalculator(question.option[0].isCorrect, 0)}
+          disabled={disable}
+          style={{ backgroundColor: bgColor.q_no === 0 && bgColor.color }}
         >
           {question?.option[0]?.text}
-        </div>
-        <div
+        </button>
+        <button
           className="qz-qn-card-optn qz-qn-card-chip chip-hover"
-          onClick={() => scoreCalculator(question.option[1].isCorrect)}
+          onClick={() => scoreCalculator(question.option[1].isCorrect, 1)}
+          disabled={disable}
+          style={{ backgroundColor: bgColor.q_no === 1 && bgColor.color }}
         >
           {question?.option[1]?.text}
-        </div>
-        <div
+        </button>
+        <button
           className="qz-qn-card-optn qz-qn-card-chip chip-hover"
-          onClick={() => scoreCalculator(question.option[2].isCorrect)}
+          onClick={() => scoreCalculator(question.option[2].isCorrect, 2)}
+          disabled={disable}
+          style={{ backgroundColor: bgColor.q_no === 2 && bgColor.color }}
         >
           {question?.option[2]?.text}
-        </div>
-        <div
+        </button>
+        <button
           className="qz-qn-card-optn qz-qn-card-chip chip-hover"
-          onClick={() => scoreCalculator(question.option[3].isCorrect)}
+          onClick={() => scoreCalculator(question.option[3].isCorrect, 3)}
+          disabled={disable}
+          style={{ backgroundColor: bgColor.q_no === 3 && bgColor.color }}
         >
           {question?.option[3]?.text}
-        </div>
+        </button>
       </div>
     </>
   );
